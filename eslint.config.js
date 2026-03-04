204483a59ca22ea9bd1082b859e1b6fd4bbb570b
Seemmaa <semahodali@gmail.com>
2026-03-04 02:50:43 +0200

    fix failure

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,25 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginSecurity from "eslint-plugin-security";
+import pluginSecurity from "eslint-plugin-security"; // 1. Import the plugin object
 
 export default tseslint.config(
-  
   {
     ignores: ["dist/**", "coverage/**", "node_modules/**"]
   },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginSecurity.configs.recommended, // 2. Add the object directly, not as a string in 'extends'
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-    ],
     languageOptions: {
-      
       globals: {
         ...globals.browser,
-        ...globals.node, 
+        ...globals.node,
       },
     },
     rules: {
-     
       "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }]
-    }
-  },
-  {
-    files: ["**/*.ts"],
-    extends: [
-      "plugin:security/recommended"
-    ],
-    rules: {
-      "security/detect-object-injection": "off"
     }
   }
 );
